bfda0d0b9b11ca95ef80a12baabf2a6be4d66a09
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 01:32:21 +0000

    Fix weird formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   
   validates :firstname, :lastname, :password_digest, presence: true
   validates :username, :email, uniqueness: true
-	validates :password, length: {minimum: 6}
-	
+  validates :password, length: {minimum: 6}
+  
 	after_initialize :ensure_session_token
 	
 	# Find the user by credentials and check if they are correct
@@ -16,14 +16,13 @@ class User < ApplicationRecord
 	  user = User.find_by(email: email)
 	  user && user.authenticate(password) ? user : nil
 	end
-	
 	# Generate a secure session token
 	def self.generate_session_token
-    SecureRandom::urlsafe_base64(16)
-  end
+	  SecureRandom::urlsafe_base64(16)
+	end
 	
 	# Helper to generate, save and return new session token
-	def reset_session_token!
+  def reset_session_token!
     self.session_token = User.generate_session_token
     self.save!
     self.session_token
