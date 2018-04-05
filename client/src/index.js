2b062e9a822a7c0007fd6544d14bcbc8b7c6d93f
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 16:46:49 +0000

    Addmodule imports

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-// import rootReducer './reducers'
+import rootReducer from './reducers'
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// The store will be imported here and assigned to:
-// createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(
-                // <Provider store={store} >
+                <Provider store={store} >
                   <App />
-                // </Provider>
+                </Provider>
                 ,
                 document.getElementById('root'));
 registerServiceWorker();
