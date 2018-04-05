cc7a761d3de2aafc2476b3ba957eec1e6cebd19a
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 17:01:38 +0000

    Add home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = (props) => {
+  return (
+    <div className='container'>
+      <div className='header-greeting'>
+        <em>AirBrB</em><h1> Book unique homes and experiences all over the world.</h1>
+      </div>
+    </div>
+    )
+}
+
+export default Home
