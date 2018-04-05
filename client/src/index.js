712cf077cd0c2da40cd99318133d9cae212dcedf
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 16:16:40 +0000

    Import modules to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+// import rootReducer './reducers'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
