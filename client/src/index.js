30a534d5acdf5f3050eeab4e4aceb5b2102b9339
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 16:26:42 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,5 +8,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// The store will be imported here and assigned to:
+// createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+ReactDOM.render(
+                // <Provider store={store} >
+                  <App />
+                // </Provider>
+                ,
+                document.getElementById('root'));
 registerServiceWorker();
