25704b854f050747e689a0a441898321da7d9bee
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 21:20:30 +0000

    Move store to its own folder

---
+++ client/src/store/store.js
@@ -0,0 +1,14 @@
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers,
+  compose
+} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers/index'
+
+const middleware = [thunk]
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+export default store
