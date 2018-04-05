51e36fe1c621b38f7932f194e6fcefed32c95eae
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 02:47:37 +0000

    Add Listing validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,15 @@
 class Listing < ApplicationRecord
+  has_many :bookings
+  has_many :reviews, through: :users
+  belongs_to :host, 
+                  class_name: 'User',
+                  foreign_key: :host_id
+  
+  
+  validates_attachment_content_type :img_url, content_type: /\Aimage\/.*\z/
+  
+  validates :host_id, :title, :description, :address, :lat, :long, :price, :prop_type, :room_type, :num_guests, :bedrooms, :beds,
+  :rules
+  
+  # IDEA: Move ameninities to another table/model?
 end
