b435b17a8404445756aaf8a71c39f50e4363a44c
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-05 14:58:22 +0000

    Add rating method to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,8 +8,18 @@ class Listing < ApplicationRecord
   
   validates_attachment_content_type :img_url, content_type: /\Aimage\/.*\z/
   
-  validates :host_id, :title, :description, :address, :lat, :long, :price, :prop_type, :room_type, :num_guests, :bedrooms, :beds,
-  :rules
+  validates :host_id, :title, :description, :address, :lat, :long, :price, :prop_type, :room_type, :num_guests, :bedrooms, :beds, :rules
+  
+  
   
   # IDEA: Move ameninities to another table/model?
+  
+  def rating
+    reviewTotal = 0
+    
+    self.reviews.each do |review|
+      reviewTotal += review.rating
+    end
+    reviewTotal.to_f / self.reviews.count
+  end
 end
