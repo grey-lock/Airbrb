8f9c36e6a0d6314e78e2ab4f8ca6ae7633bbc063
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-06 19:23:34 +0000

    Add listing actions

---
+++ client/src/actions/listing_actions.js
@@ -0,0 +1,46 @@
+import fetch from 'isomorphic-fetch'
+
+const API_URL = process.env.REACT_APP_API_URL
+
+// Assign this to clean up fetch code
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
+
+// Action Creators
+export const getListings = listings => {
+  console.log('In action')
+  return {
+    type: 'GET_LISTINGS',
+    listings
+  }
+}
+
+export const getListing = listing => {
+  console.log('In action')
+  return {
+    type: 'GET_LISTING',
+    listing
+  }
+}
+
+export const addListing = listing => {
+  console.log('In action')
+  return {
+    type: 'ADD_LISTING',
+    listing
+  }
+}
+
+// Async Actions
+
+export const fetchListings = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/listings`, { headers })
+    .then(resp => resp.json())
+    .then(listings => dispatch(getListings(listings)))
+    .catch(error => console.log(error));
+  }
+}
+
