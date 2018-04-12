f30c9727c4cc430521d4f290cf7aa9e6a06ba256
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-12 02:57:05 +0000

    Fix typo

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -4,7 +4,7 @@ const AboutPage = props => (
   <div className="about">
     <div className="container">
       <h2>About</h2>
-      <p>Airbrb is a full stack applications inspired by Airbnb. The front end is made with JavaScript, utilizing the React & Redux frameworks. The back end is created with Ruby on Rails. The app is styled with an open source Bootstrap theme with custom CSS</p>
+      <p>Airbrb is a full stack application inspired by Airbnb. The front end is made with JavaScript, utilizing the React & Redux frameworks. The back end is created with Ruby on Rails. The app is styled with an open source Bootstrap theme with custom CSS</p>
       <h3>Curious about the code in this project?</h3>
         <div className="icon-box">
           <a href="https://github.com/TheInvalidNonce/AirBrB" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
