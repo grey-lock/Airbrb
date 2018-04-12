d66cbd5de6feac40283036623cc19539f386db03
Janusz Szubert <theinvalidnonce@gmail.com>
2018-04-12 02:49:27 +0000

    Breakthrough: Gmaps container is finally aligned on the right

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,12 +9,13 @@ const Home = (props) => {
       <div className='header-greeting'>
         <h1><strong>Airbrb</strong> <br/>Book unique homes and experiences all over the world.</h1>
       </div>
-      <div className='col-md-8'>
-        <ListingsContainer className='col-md-8'/>
-      </div>
       <div className='col-md-4 map-search-container'>
         <Map className='col-md-4'/>
       </div>
+      <div className='col-md-8'>
+        <ListingsContainer className='col-md-8'/>
+      </div>
+
       
       
     </div>
