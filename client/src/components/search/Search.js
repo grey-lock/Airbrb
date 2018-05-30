d3b7e32c4b5216be4bdbcc957ae92f9ef9977f9d
Janusz Szubert <theinvalidnonce@gmail.com>
2018-05-29 22:14:55 -0400

    Add search form containers

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { fetchListings } from '../../actions/listing_actions.js'
 
 const VALID_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/'];
 
@@ -33,12 +34,46 @@ class Search extends React.Component {
 
   handleOnSubmit(e) {
     e.preventDefault()
+
+    this.props.fetchListings({
+      address: this.state.address,
+      checkIn: this.state.checkIn,
+      checkOut: this.state.checkOut,
+    })
   }
 
   render() {
     return(
       <div id='search'>
-
+        <form onSubmit={ this.handleOnSubmit }>
+          <div className='search-form-inner-wrapper'>
+            <button display='none' type='submit'></button>
+            <div className='search-input-wrapper'>
+              <input
+                type='text' 
+                value={ this.state.address }
+                onChange={ this.handleAddressChange }
+                placeholder='Search listings'
+              />
+            </div>
+            <div className='search-input-wrapper'>
+              <input 
+                type='text' 
+                value={ this.state.checkIn }
+                onChange={ this.handleDateChange('checkIn') }
+                placeholder='Check In (mm/dd/yy)'
+              />
+            </div>
+            <div className='search-input-wrapper'>
+              <input 
+                type='text' 
+                value={ this.state.checkOut }
+                onChange={ this.handleDateChange('checkOut') }
+                placeholder='Check Out (mm/dd/yy)'
+              />
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
